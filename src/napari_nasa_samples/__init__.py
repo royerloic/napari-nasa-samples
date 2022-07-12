2576de39cc2295c2cb9816231753113ff7ae2088
Loic A. Royer <royerloic@gmail.com>
2022-07-12 19:41:28 +0200

    initial commit

---
+++ src/napari_nasa_samples/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.1"
+from ._sample_data import make_sample_data
+
+__all__ = (
+    "make_sample_data",
+    )
---
+++ src/napari_nasa_samples/_tests/test_sample_data.py
@@ -0,0 +1,7 @@
+# from napari_nasa_samples import make_sample_data
+
+# add your tests here...
+
+
+def test_something():
+    pass
