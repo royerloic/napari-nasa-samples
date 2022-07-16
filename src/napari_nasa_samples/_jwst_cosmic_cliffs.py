ccd0361cba6062ee03bb5682152a06daa705416a
Loic A. Royer <royerloic@gmail.com>
2022-07-16 07:37:00 -0700

    fixed url.

--- src/napari_nasa_samples/_jwst_cosmic_cliffs.py
+++ src/napari_nasa_samples/_jwst_cosmic_cliffs.py
@@ -7,6 +7,6 @@ from napari_nasa_samples.utils import get_array
 
 def make_sample_data():
     """Generates an image"""
-    array = get_array("https://stsci-opo.org/STScI-01G7ETNMR8CBHQQ64R4CVA1E6T.tif")
+    array = get_array("https://stsci-opo.org/STScI-01G7WCH0VH9RK7YBFAMM07S2TB.tif")
     return [(array, {'name':'Cosmic Cliffs, Carina Nebula, JWST'})]
 
